fdc433dd69a27c473ec55a4b438d33cadf4367bb
Asmaa Abbas <asmaa@flatironschool.com>
2018-08-09 23:50:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ function scuberGreetingForFeet (ride) {
   if (ride < 400) {
     return 'This one is on me!';
   } else if (ride > 3000) {
-    return ''
+    return 'I will gladly take your thirty bucks.';
   }
 }
