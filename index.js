7b0a963783e9f598569a3007ffc0278316b0a82c
Asmaa Abbas <asmaa@flatironschool.com>
2018-08-10 00:07:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,9 @@ function ternaryCheckCity(city) {
 function switchOnCharmFromTip(tip) {
   switch (tip){
     case ('generous'):
-    return 'Thank you so much.';
+      return 'Thank you so much.';
+    case (''):
+      return 
   }
     
 }
